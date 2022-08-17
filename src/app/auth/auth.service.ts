e4cb8c904ae6fcf9b6bac754cbad473b9aa3bf2b
Miklos Gabor <gmiki91@gmail.com>
2022-08-17 22:55:53 +0200

    passwordmatch, typo fix, url

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,14 +1,16 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { environment } from "src/environments/environment";
 import { User } from "../models/user.model";
 
 @Injectable({
     providedIn:'root'
 })
+
 export class AuthService{
     constructor(private http: HttpClient) {}
 
     signUp(user:User){
-        this.http.post("http://localhost:3000/api/users/signup",user).subscribe(result=>{console.log(result)})
+        this.http.post(`${environment.url}/users/signup`,user).subscribe(result=>{console.log(result)})
     }
 }
