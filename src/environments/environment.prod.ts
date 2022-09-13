e6a016280e0ce70e8e18719a6b276be29b241680
Miklos Gabor <gmiki91@gmail.com>
2022-09-13 15:13:16 +0200

    prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url:'https://f5aztfr90g.execute-api.eu-central-1.amazonaws.com/dev/api'
+  url:'https://z3amy63uz2.execute-api.eu-central-1.amazonaws.com/dev/api'
 };
